6dfc50c030de393fdded793a2551f10ec7601ca8
Maxim Mitin <maximus405@gmail.com>
2021-12-25 21:24:08 +0300

    create MVC blueprint for refactoring via MVVM model

---
+++ MVVM_test/Profile.swift
@@ -0,0 +1,15 @@
+//
+//  Profile.swift
+//  MVVM_test
+//
+//  Created by Maxim Mitin on 25.12.21.
+//
+
+import Foundation
+
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
